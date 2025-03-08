ad9c774283a2f3bdbdc495ba633c81be40714645
onelrian <onelsob57@gmail.com>
2025-03-08 12:44:44 +0100

    CI: Added the Base64 Encoding to the Payload

--- src/query.rs
+++ src/query.rs
@@ -4,7 +4,10 @@ use std::{
     net::TcpStream,
 };
 
-use crate::response::send_response;
+use crate::{
+    base64_decode::base64_decode,
+    response::{self, send_response},
+};
 
 pub fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
@@ -15,19 +18,21 @@ pub fn handle_client(mut stream: TcpStream) {
             if bytes_read == 0 {
                 eprintln!("No data recieved from Client");
             }
-            
-                // Converting the buffer to String
-                let request = match str::from_utf8(&buffer[..bytes_read]){
-                    Ok(req) => req,
-                    Err(e) => {
-                        eprintln!("Failed To convert buffer to UTF-8: {}", e);
-                        let response = "HTTP/1.1 404 Bad Request\r\n\r\nError: Invalid UTF-8 Encoding.";
-                        send_response(&mut stream, response);
-                        return;
-                    }
-                };
-            
-                println!("Received Request: \n{}", request);
+
+            // Converting the buffer to String
+            let request = match str::from_utf8(&buffer[..bytes_read]) {
+                Ok(req) => req,
+                Err(e) => {
+                    eprintln!("Failed To convert buffer to UTF-8: {}", e);
+                    let response = "HTTP/1.1 404 Bad Request\r\n\r\nError: Invalid UTF-8 Encoding.";
+                    send_response(&mut stream, response);
+                    return;
+                }
+            };
+
+            println!("Received Request: \n{}", request);
+            // check if Request is a Post for decoding
+            if request.starts_with("POST /decode HTTP/1.1") {
                 // Screening The Request Line
                 let request_line = match request.lines().next() {
                     Some(line) => line,
@@ -38,48 +43,48 @@ pub fn handle_client(mut stream: TcpStream) {
                         return;
                     }
                 };
-            
+
                 // Extract The query string From the Request line
                 let query_path = match request_line.split_whitespace().nth(1) {
                     Some(qp) => qp,
                     None => "",
                 };
-            
+
                 let query_string = match query_path.split('?').nth(1) {
                     Some(qs) => qs,
                     None => "",
                 };
-            
+
                 // Parsing the Query Parameters
-            
+
                 let mut delay: Option<u64> = None;
                 let payload: Option<String> = None;
-            
+
                 for pair in query_string.split('&') {
                     let mut key_value = pair.split('=');
                     let key = match key_value.next() {
                         Some(k) => k,
                         None => "",
                     };
-            
+
                     let value = match key_value.next() {
                         Some(val) => val,
                         None => "",
                     };
-            
+
                     match key {
                         "delay" => {
                             delay = value.parse::<u64>().ok();
                         }
-            
+
                         "payload" => {
                             value.to_string();
                         }
-            
+
                         _ => {}
                     }
                 }
-            
+
                 // Validating Payload and Delay
                 let delay = match delay {
                     Some(d) if d > 0 => d,
@@ -90,7 +95,7 @@ pub fn handle_client(mut stream: TcpStream) {
                         return;
                     }
                 };
-            
+
                 let payload = match payload {
                     Some(p) if !p.trim().is_empty() => p,
                     _ => {
@@ -100,7 +105,27 @@ pub fn handle_client(mut stream: TcpStream) {
                         return;
                     }
                 };
-            
+
+                let decoded_payload = match base64_decode(&payload) {
+                    Ok(data) => data,
+                    Err(e) => {
+                        let response =  "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}";
+                        send_response(&mut stream, response);
+                        return;
+                    }
+                };
+
+                // Convert the decoded payload back to a string (for simplicity)
+                let decoded_message = String::from_utf8_lossy(&decoded_payload);
+
+                // Construct the response
+                let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{{\"decoded_message\": \"{}\"}}",
+            decoded_message.len() + 22, /* 22 is the length of the JSON structure and quotes */
+            decoded_message
+        );
+
+                send_response(&mut stream, &response);
                 // Assuming Parameters are Valid
                 println!(
                     "Received valid Request: delay={} , payload={}",
@@ -113,11 +138,15 @@ pub fn handle_client(mut stream: TcpStream) {
                 if let Err(e) = stream.flush() {
                     eprintln!("Failed to Flush response: {}", e)
                 }
-        },
+            } else {
+                // Construct a 404 respnse
+                let response =  "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found";
+                send_response(&mut stream, response);
+            }
+        }
 
         Err(e) => {
-            eprintln!("Failed to read form Stream: {}", e);    
+            eprintln!("Failed to read form Stream: {}", e);
         }
     }
-
 }
