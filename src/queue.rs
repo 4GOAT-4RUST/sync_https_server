500d3fed67814f8b04f6cf4bc052ee732ae257c2
Donemmanuelo <emmanuelodon943@gmail.com>
2025-03-09 16:26:05 +0100

    just made some little changes

--- src/queue.rs
+++ src/queue.rs
@@ -1,11 +1,39 @@
 use std::collections::VecDeque;
-pub fn upcomming_input(input: String) -> String {
-    let mut queue: VecDeque<String> = VecDeque::new();
-    queue.push_back(input);
-  let item = "Hello";
-    for _ in 0..8 {
-        let item = queue[0].clone();
-        queue.pop_front();
+use std::sync::{Arc, Mutex};
+use std::thread;
+type Task = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    tasks: Arc<Mutex<VecDeque<Task>>>,
+}
+impl ThreadPool {
+    pub fn new(size: usize, workers: Vec<thread::JoinHandle<()>> ) -> Self {
+        //let size = 8;
+        let tasks = Arc::new(Mutex::new(VecDeque::<Task>::new()));
+
+        for _ in 0..size {
+            let tasks = Arc::clone(&tasks);
+            let worker: thread::JoinHandle<()> = thread::spawn(move || loop {
+                let task = tasks.lock().unwrap().pop_front();
+                if let Some(task) = task  {
+                    task();
+                }
+             
+
+            });
+
+            workers.push(worker);
+
+        }    
+        ThreadPool { tasks, workers }
     }
-   item.to_string()
+}
+impl ThreadPool {
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let mut tasks = self.tasks.lock().unwrap();
+        tasks.push_back(Box::new(f));
+    }
 }
