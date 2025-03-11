d71bd844c94cb03cb60c1de8455478a4840dd817
jagoum <jagoum24@gmail.com>
2025-03-11 10:33:24 +0100

    Added test for threadpool

---
+++ src/log.rs
@@ -0,0 +1,9 @@
+use std::fmt;
+
+pub fn log_success<T: fmt::Display>(message:T){
+    println!("[Success]: {}",message);
+}
+
+pub fn log_failure<F: fmt::Display>(message: F) {
+    eprintln!("[Failure]: {}",message);
+}
