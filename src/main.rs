69ede291ebb767cae825470c0e7415c025017b7a
onelrian <onelsob57@gmail.com>
2025-03-19 03:21:44 +0100

    Refactor: improved error handling in main by catching propagated RequestError from functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::net::TcpListener;
-
 use sync_https_server::{query::handle_client, threadpool::ThreadPool};
 
 fn main() {
@@ -10,7 +9,7 @@ fn main() {
     println!("############################################################");
 
     let listener = match TcpListener::bind("0.0.0.0:8080") {
-        Ok(tcp_listener) => tcp_listener, // Successfully binds
+        Ok(tcp_listener) => tcp_listener,
         Err(e) => {
             eprintln!("Error binding to port: {}", e);
             return;
@@ -18,24 +17,27 @@ fn main() {
     };
     println!("Server listening on 127.0.0.1:8080");
 
-    let threadpool = ThreadPool::new(8);
+    let threadpool = match ThreadPool::new(8) {
+        Ok(pool) => pool,
+        Err(e) => {
+            eprintln!("ThreadPool initialization failed: {}", e);
+            return;
+        }
+    };
 
     for stream in listener.incoming() {
         let stream = match stream {
             Ok(s) => s,
             Err(e) => {
-                eprintln!("Failed To Established Connection: {}", e);
+                eprintln!("Failed To Establish Connection: {}", e);
                 continue;
             }
         };
 
-        match threadpool {
-            Ok(ref threadpool) => {
-                threadpool.execute(move || handle_client(stream));
+        threadpool.execute(move || {
+            if let Err(e) = handle_client(&stream) {
+                eprintln!("Error handling client: {}", e);
             }
-            Err(_) => {
-                eprintln!("Could Not Execute threadpool")
-            }
-        };
+        });
     }
 }
